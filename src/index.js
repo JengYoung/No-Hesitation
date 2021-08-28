f3945ba5fd7203fca42355046161f9769462ec3c
Jengyoung <young_pallete@naver.com>
2021-08-28 19:44:29 +0900

    refactor: eslint 설정 변경

--- src/index.js
+++ src/index.js
@@ -2,4 +2,4 @@ import App from './App.js';
 new App({
   $target: document.querySelector('#app'),
 });
-document.querySelector('#app').innerHTML = '<div>Hello, world!</div>'
+document.querySelector('#app').innerHTML = '<div>Hello, world!</div>';
