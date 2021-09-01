621ac80c84009d5a1fd4e18c622ae87d215745a8
Jengyoung <young_pallete@naver.com>
2021-09-01 13:40:30 +0900

    fix: HtmlPlugin 설정을 통한 빌드 정상 동작 확인

--- webpack.config.cjs
+++ webpack.config.cjs
@@ -1,10 +1,15 @@
 const path = require('path');
-
+const HtmlPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
 module.exports = {
   entry: ['@babel/polyfill', './src/index.js'],
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: 'bundle.js',
+  },
+  devServer: {
+    static: './dist',
+    hot: true,
   },
   module: {
     rules: [
@@ -20,8 +25,34 @@ module.exports = {
           },
         },
       },
+      {
+        test: /\.png$/, // .png 확장자로 마치는 모든 파일
+        loader: 'file-loader',
+        options: {
+          publicPath: './dist/', // prefix를 아웃풋 경로로 지정
+          name: '[name].[ext]?[hash]', // 파일명 형식
+        },
+      },
+      {
+        test: /\.png$/,
+        use: {
+          loader: 'url-loader', // url 로더를 설정한다
+          options: {
+            publicPath: './dist/', // file-loader와 동일
+            name: '[name].[ext]?[hash]', // file-loader와 동일
+            limit: 5000 // 5kb 미만 파일만 data url로 처리
+          }
+        }
+      }
     ],
   },
+  plugins: [
+    new HtmlPlugin({
+      template: './index.html',
+      hash: true,
+    }),
+    new CleanWebpackPlugin(),
+  ],
   devtool: 'source-map',
   mode: 'development',
 };
